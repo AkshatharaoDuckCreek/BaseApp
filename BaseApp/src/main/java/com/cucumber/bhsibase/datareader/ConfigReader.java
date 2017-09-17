9aa66f283d1a14ac6c3d947eb217ac1851b11a20
Akshatha <Akshatha@192.168.1.6>
2017-09-17 23:33:07 +0530

    Removed "" fron config.properties CHanged the logic of ConfigReader.java Added an xml file to save data And added log4j in partybase

--- BaseApp/src/main/java/com/cucumber/bhsibase/datareader/ConfigReader.java
+++ BaseApp/src/main/java/com/cucumber/bhsibase/datareader/ConfigReader.java
@@ -7,37 +7,23 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Properties ; 
+import java.util.Properties;
 
 public class ConfigReader {
-	 public String readurls(String applicaton) throws IOException {
 
-			Properties prop = new Properties();
-			InputStream input = null;
-			String url = "" ; 
+	private static final String FILENAME = "com/cucumber/bhsibase/config/config";
+	private static ResourceBundle resourceBundle = ResourceBundle.getBundle(FILENAME);
 
-		
-
-				//input = new FileInputStream("com/cucumber/bhsibase/config/config.properties");
-
-				// load a properties file
-			//	prop.load(input);
-				final String FILENAME = "com/cucumber/bhsibase/config/config";
-				
-				ResourceBundle s=ResourceBundle.getBundle(FILENAME);
-		        s.getString(applicaton);
-				
-				if(applicaton=="ExpressURL"){
-					System.out.println("application:::::::::"+applicaton);
-				 url = prop.getProperty("ExpressURL");
-				 System.out.println("url::::::::::::::::::::::"+url);
-				
-				}
-				 return url ; 
-			} 
-			
-		
-	
+	public static String readurls(final String applicaton) {
+		// Properties prop = new Properties();
+		String url = null;
+		if (resourceBundle != null) {
+			url = resourceBundle.getString(applicaton);
+		} else {
+			//need to add logs 
 		}
-	
 
+		return url;
+	}
+
+}
--- BaseApp/src/main/java/com/cucumber/bhsibase/testbase/PartyBase.java
+++ BaseApp/src/main/java/com/cucumber/bhsibase/testbase/PartyBase.java
@@ -3,7 +3,6 @@ package com.cucumber.bhsibase.testbase;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,33 +13,20 @@ import cucumber.api.java.en.When;
 
 public class PartyBase {
 
-	
-public static WebDriver driver ; 
- String url = "https://pcdev03.accs54683.asw.accenture.com:8080/BASE_Express/default.aspx" ;
-String browser = "firefox" ; 
-ConfigReader reader = new ConfigReader() ; 
+	public static WebDriver driver;
+	String url = "https://pcdev03.accs54683.asw.accenture.com:8080/BASE_Express/default.aspx";
+	String browser = "firefox";
+	ConfigReader reader = new ConfigReader();
 
+	public void geturl() throws Exception {
 
+		driver = new FirefoxDriver();
 
+		driver.get(reader.readurls("ExpressURL"));
+		String log4jConfPath = "log4j.properties" ; 
 
-/**public void selectBrowser(String browser){
-	if(browser.equalsIgnoreCase("friefox")){
-		
-		driver = new FirefoxDriver() ; 
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 	}
-	
-}
- * @throws Exception */
-
-public void geturl() throws Exception{
-	
-	driver = new FirefoxDriver() ; 
-	System.out.println("UUUUUUUUURRRRRRRRLLLLLLLL222222222222222222::::::::::::"+url);
-	driver.get(reader.readurls("ExpressURL"));
-	System.out.println("UUUUUUUUURRRRRRRRLLLLLLLL33333333333333333333::::::::::::"+url);
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS) ; 
-}
-	
 
 }
