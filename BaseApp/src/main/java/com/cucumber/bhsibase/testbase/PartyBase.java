aaa36c5950fdba3c66238085ef26844deab28c92
akshatha.b.rao <akshatha.b.rao@MUM-28R3FC2.duckcreek.com>
2017-09-22 15:52:35 +0530

    modified log4j.properties and PartyBase.java

--- BaseApp/src/main/java/com/cucumber/bhsibase/testbase/PartyBase.java
+++ BaseApp/src/main/java/com/cucumber/bhsibase/testbase/PartyBase.java
@@ -3,6 +3,7 @@ package com.cucumber.bhsibase.testbase;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,16 +15,22 @@ import cucumber.api.java.en.When;
 public class PartyBase {
 
 	public static WebDriver driver;
+
+	final static Logger logger = Logger.getLogger(PartyBase.class);
+
 	String url = "https://pcdev03.accs54683.asw.accenture.com:8080/BASE_Express/default.aspx";
 	String browser = "firefox";
 	ConfigReader reader = new ConfigReader();
 
 	public void geturl() throws Exception {
 
+		logger.info("Entering the execute message");
+
 		driver = new FirefoxDriver();
+		logger.info("Mozzilla Browser opens");
 
 		driver.get(reader.readurls("ExpressURL"));
-		String log4jConfPath = "log4j.properties" ; 
+		logger.info("Navigate to express");
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
